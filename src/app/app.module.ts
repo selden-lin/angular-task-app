e4c469fc55a9a6851e44c424976d46b49625342c
Selden Lin <selden.lin@gmail.com>
2020-09-12 21:21:05 -0400

    reinitialized repo with scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { SingleTaskListComponent } from './single-task-list/single-task-list.component';
-import { TaskTimeGraphComponent } from './task-time-graph/task-time-graph.component';
-import { UpdateTaskInfoComponent } from './update-task-info/update-task-info.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    DashboardComponent,
-    SingleTaskListComponent,
-    TaskTimeGraphComponent,
-    UpdateTaskInfoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
