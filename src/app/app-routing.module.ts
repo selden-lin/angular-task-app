e3075ce69f2f21ceed972f38e31df3393c77c8a5
Selden Lin <selden.lin@gmail.com>
2020-09-12 21:43:03 -0400

    added basic routing and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {TimeGraphComponent} from './time-graph/time-graph.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        component: DashboardComponent
+    },
+    {
+        path: 'timeGraph',
+        component: TimeGraphComponent
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { SingleListViewComponent } from './single-list-view/single-list-view.component';
 import { TimeGraphComponent } from './time-graph/time-graph.component';
 import { TaskViewComponent } from './task-view/task-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +24,18 @@ import { TaskViewComponent } from './task-view/task-view.component';
     DashboardComponent,
     SingleListViewComponent,
     TimeGraphComponent,
-    TaskViewComponent
+    TaskViewComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
